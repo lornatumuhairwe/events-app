2b1b92d55567b2445a806b56d76a16d2bdefaab3
Lorna Tumuhairwe <lornatumuhairwe@gmail.com>
2018-05-14 14:39:21 +0300

    devise login helper

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,12 +3,19 @@ require 'rails_helper'
 RSpec.describe HomeController do
     let(:user) { instance_double(User) }
 
-    before { log_in(user) }
-
     describe "GET #index" do
         it "returns status ok" do
+            log_in(user)
+
             get :index
             expect(response.status).to be(200)
+        end
+
+        it "blocks unauthenticated user" do
+            log_in nil
+
+            get :index
+            expect(response.status).to be(302)
         end
     end
 end
--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,8 +1,13 @@
 module ControllerHelpers
     def log_in(user)
         warden = request.env['warden']
-
-        allow(warden).to receive(:authenticate!).and_return(user)
-        allow(controller).to receive(:current_user).and_return(user)
+        if user
+            allow(warden).to receive(:authenticate!).and_return(user)
+            allow(controller).to receive(:current_user).and_return(user)
+        else
+            allow(warden).to receive(:authenticate!).and_throw(:warden, {:scope => :user})
+            allow(controller).to receive(:current_user).and_return(nil)
+        end
     end
 end
+# https://github.com/plataformatec/devise/wiki/How-To:-Stub-authentication-in-controller-specs
